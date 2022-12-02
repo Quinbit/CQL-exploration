8825463dca1080aaa29dad3d52f604039b2eee32
sbiswas <sbiswas@waabi.ai>
2022-12-01 21:52:27 -0500

    add unexplored baseline

--- SimpleSAC/sampler.py
+++ SimpleSAC/sampler.py
@@ -211,6 +211,97 @@ class OurSampler(object):
     def env(self):
         return self._env
 
+class CQLUnexploredSampler(object):
+    
+    def __init__(self, env, max_traj_length=1000, qf1=None, qf2=None, device='cuda'):
+        self.max_traj_length = max_traj_length
+        self._env = env
+        self._traj_steps = 0
+        self._current_observation = self.env.reset()
+        self.qf1 = qf1
+        self.qf2 = qf2
+        self.device = device
+        self.n_samples = 10
+        self.n_ensemble_samples = 10
+        self.std = 0.1
+        self.state_action_history = None
+        self.max_history_length = 10000
+
+    def sample(self, policy, n_steps, deterministic=False, replay_buffer=None):
+        observations = []
+        actions = []
+        rewards = []
+        next_observations = []
+        dones = []
+        action_q_diffs = []
+
+        for _ in range(n_steps):
+
+            self._traj_steps += 1
+            observation = self._current_observation
+            action = policy(
+                np.expand_dims(observation, 0), deterministic=deterministic
+            )[0, :]
+
+            observation_tensor = torch.tensor(observation, dtype=torch.float32, device=self.device)
+            action_tensor = torch.tensor(action, dtype=torch.float32, device=self.device)
+            
+            next_action = torch.zeros_like(action_tensor)
+            max_diff = 0
+            for _ in range(self.n_samples):
+                new_action = action_tensor + torch.randn(action_tensor.shape, device=action_tensor.device) * self.std
+                qf1_vals = self.qf1(observation_tensor, new_action)
+                qf2_vals = self.qf2(observation_tensor, new_action)
+
+                new_observation, _, _, _ = self.env.step(new_action.detach().cpu().numpy())
+
+                state_action_new_vec = np.concatenate((new_observation, new_action)).reshape(1, -1)
+                if self.state_action_history is None:
+                    self.state_action_history = np.concatenate((observation, action)).reshape(1, -1)
+
+                # dist to existing state/action clusters - if dist is large then it's more unexplored
+                action_q_diff = np.min(np.linalg.norm(self.state_action_history - np.tile(state_action_new_vec, len(self.state_action_history)).reshape(len(self.state_action_history), -1), axis=1))
+                if action_q_diff > max_diff:
+                    max_diff = action_q_diff
+                    next_action = new_action
+
+            next_observation, reward, done, _ = self.env.step(next_action.detach().cpu().numpy())
+
+            self.state_action_history = np.concatenate((self.state_action_history, np.concatenate((next_observation, next_action)).reshape(1, -1)))
+            if len(self.state_action_history) > self.max_history_length:
+                self.state_action_history = self.state_action_history[len(self.state_action_history) - self.max_history_length:]
+
+            observations.append(observation)
+            actions.append(action)
+            rewards.append(reward)
+            dones.append(done)
+            next_observations.append(next_observation)
+            action_q_diffs.append(action_q_diff.item())
+
+            if replay_buffer is not None:
+                replay_buffer.add_sample(
+                    observation, action, reward, next_observation, done
+                )
+
+            self._current_observation = next_observation
+
+            if done or self._traj_steps >= self.max_traj_length:
+                self._traj_steps = 0
+                self._current_observation = self.env.reset()
+
+        d = dict(
+            observations=np.array(observations, dtype=np.float32),
+            actions=np.array(actions, dtype=np.float32),
+            rewards=np.array(rewards, dtype=np.float32),
+            next_observations=np.array(next_observations, dtype=np.float32),
+            dones=np.array(dones, dtype=np.float32),
+        )
+        return d
+
+    @property
+    def env(self):
+        return self._env
+
 
 class EnsembleSampler(object):
     
